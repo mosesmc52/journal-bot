03bd0eb364e22b33922e7fa2c01ceb42f9f7f4f3
Moses McCall <mosesmc52@gmail.com>
2024-02-05 21:24:25 -0500

    remove entry point

--- entrypoint.js
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run Celery worker
-celery -A app.celery worker --loglevel=INFO --detach --pidfile=''
-
-# Run Celery Beat
-celery -A app.celery beat --loglevel=INFO --detach --pidfile=''
-
-flask run --host=0.0.0.0 --port 5000
